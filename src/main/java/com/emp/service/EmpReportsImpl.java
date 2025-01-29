77b160c3afa39ecd502f42a0c8e2d393ead518a0
manohar <manohar_shett@yahoo.com>
2025-01-29 15:06:12 -0500

    generate employee reports

--- src/main/java/com/emp/service/EmpReportsImpl.java
+++ src/main/java/com/emp/service/EmpReportsImpl.java
@@ -3,6 +3,7 @@ package com.emp.service;
 import com.emp.model.Emp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -11,21 +12,32 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
 
+@Service
 public class EmpReportsImpl implements EmpReports {
     private static final Logger LOGGER = LoggerFactory.getLogger(EmpReportsImpl.class);
 
+    private static void writeToFile(File file, List<String> results) {
+        try (FileWriter writer = new FileWriter(file)) {
+            for (String result : results) {
+                writer.write(result + System.lineSeparator());
+            }
+        } catch (IOException e) {
+            LOGGER.error("Serious error while generating employee reports", e);
+        }
+    }
+
     @Override
-    public File generateReport(List<Emp> empList, String fileName)  {
+    public File generateReport(List<Emp> empList, String fileName) {
 
         ExecutorService executor = Executors.newFixedThreadPool(10);
         File outputFile = new File(fileName);
         List<Future<String>> futures = new ArrayList<>();
 
         try {
-            for (Emp e : empList) {
+            for (Emp emp : empList) {
                 Callable<String> callable = () -> {
                     return
-                            e.getEmpBasic().getFirstName() + "," + e.getEmpBasic().getLastName();
+                            emp.getEmpBasic().getFirstName() + "," + emp.getEmpBasic().getLastName();
                 };
                 futures.add(executor.submit(callable));
             }
@@ -36,7 +48,7 @@ public class EmpReportsImpl implements EmpReports {
                     results.add(future.get());
                 } catch (InterruptedException | ExecutionException e) {
 
-                    LOGGER.error("Serious error while generating employee reports",e);
+                    LOGGER.error("Serious error while generating employee reports", e);
 
                     for (Future<String> f : futures) {
                         f.cancel(true);
@@ -49,19 +61,12 @@ public class EmpReportsImpl implements EmpReports {
             }
 
             writeToFile(outputFile, results);
+        } catch (Exception e) {
+            LOGGER.error("Serious error while generating employee reports", e);
+
         } finally {
             executor.shutdown(); // Ensure executor shutdown
         }
         return outputFile;
-    }
-
-    private static void writeToFile(File file, List<String> results)  {
-        try (FileWriter writer = new FileWriter(file)) {
-            for (String result : results) {
-                writer.write(result + System.lineSeparator());
-            }
-        } catch (IOException e) {
-            LOGGER.error("Serious error while generating employee reports",e);
-        }
     }
 }
